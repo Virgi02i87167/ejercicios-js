9c19605aacd70bd3922d6f1b434aeecccf17c098
Virgi02i87167 <131634183+Virgi02i87167@users.noreply.github.com>
2024-09-16 22:42:33 -0600

    primer commit

---
+++ ejercicios.js
@@ -0,0 +1,72 @@
+// let contador = 0
+// contador = contador + 1
+// contador = contador + 1
+
+// const contador = 1;
+
+// if(true) {
+//     const contador = 5
+//     console.log(contador)
+// }
+
+// console.log(contador)
+
+// const persona = {
+//     nombre: "Virgilio",
+//     apellido: "Alfaro"
+// }
+
+// persona.nombre = "Jonathan";
+
+// console.log(persona.nombre)
+
+// Objetos literales y Optional chaining operator
+// const frutas = ["pera", "manzana", "sandia"]
+// console.log(frutas[1])
+
+// Objetos liteales, no indexados
+// const animal = {
+//     color: "naranja",
+//     nombre: "Simba",
+//     rugiendo: true,
+//     favoritos: {
+//         dia: {
+//             ok: true
+//         },
+//         noche: {
+//             ok: false
+//         }
+//     }
+// }
+
+// // Guardar el valor de la propiedad color en una constante
+// const color = animal.color;
+// const nombre = animal.nombre;
+
+// console.log("color: " + color + ", animal: " + nombre)
+
+// const tareas = {
+//     "2359-91k83204u23rhrefiu": {
+//         title: "tarea #01"
+//     }
+// }
+// console.log(tareas["2359-91k83204u23rhrefiu"].title);
+
+// const animal = {
+//     color: "azul",
+//     nombre: "Toogoz",
+//     volando: true
+// }
+
+// // destruturing objetos literales...
+// const {color, nombre, volando} = animal;
+// console.log('El ' + nombre + ' es de color: ' + color)
+
+// // arrays indexados
+// const frutas = ['peras', 'manzana', 'sandia'];
+
+// // destructuring array
+// const [pera, manzana, sandia] = frutas;
+// console.log("A mi me gusta mucho la "+ pera)
+
+// Interpolacion, Operador ternario
